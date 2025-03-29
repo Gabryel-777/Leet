7f6de539e42235993c80a6bf35399a0400413104
Gabryel-777 <gabryeldasilvalemos@gmail.com>
2025-03-28 22:39:53 -0300

    mandioca

--- Program.cs
+++ Program.cs
@@ -1,44 +1,14 @@
-﻿Console.Clear();
-Console.Write(" Se você esta lendo isto"
-.Replace("e", "3")
-.Replace("S" , "5")
-.Replace("s", "5")
-.Replace("o", "0") 
-.Replace("a", "4")
-.Replace ("t", "7")
-.Replace ("i", "1")
-.Replace("l", "1"));
-Thread.Sleep(1500);
-Console.WriteLine (" Significa que você esta me devendo uma coxinha"
-.Replace("e", "3")
-.Replace("S" , "5")
-.Replace("s", "5")
-.Replace("o", "0") 
-.Replace("a", "4")
-.Replace ("t", "7")
-.Replace ("i", "1"));
-Thread.Sleep (2500);
-Console.WriteLine(" Caso você queira se livrar desta maldição digite ALELUIA"
-.Replace("E", "3")
-.Replace("e", "3")
-.Replace("S" , "5")
-.Replace("s", "5")
-.Replace("o", "0") 
-.Replace("a", "4")
-.Replace ("t", "7")
-.Replace ("i", "1")
-.Replace ("I", "1")
-.Replace ("L", "1")
-.Replace ("l", "1")
-.Replace("A", "4")
-.Replace("E", "3"));
-Console.ReadLine();
+﻿string leet;
 
-Console.WriteLine ("Pronto você foi livrado da maldição, agora vaza"
+Console.Clear();
+Console.Write(" Escreva :");
+
+leet = Console.ReadLine()! 
 .Replace("e", "3")
 .Replace("S" , "5")
 .Replace("s", "5")
 .Replace("o", "0") 
 .Replace("a", "4")
 .Replace ("t", "7")
-.Replace ("i", "1"));
+.Replace ("i", "1")
+.Replace("l", "1");
